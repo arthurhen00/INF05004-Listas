21b4d7fefe9fb7135bb08f6bc22d486e44fda810
arthur <arthurhendges@hotmail.com>
2024-10-11 10:43:57 -0300

    manhattandistance, incomplete

--- lista1/main.cpp
+++ lista1/main.cpp
@@ -9,6 +9,12 @@ bool IsGoal(const std::vector<int>& state) {
 
 int main() {
     printf("Hello, World!\n");
+    ManhattanDistance heuristic;
+
+    printf("expected: 1 , res: [%d]\n", heuristic.Calculate({1, 0, 2, 3, 4, 5, 6, 7, 8}));
+    printf("expected: 6 , res: [%d]\n", heuristic.Calculate({5, 1, 2, 0, 4, 3, 6, 7, 8}));
+    printf("expected: 20, res: [%d]\n", heuristic.Calculate({8, 7, 6, 5, 4, 3, 2, 1, 0}));
+
 
     return EXIT_SUCCESS;
 }
