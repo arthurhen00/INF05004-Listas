ce4563c9678ef07ea925f76ec28698b8df0e2421
arthur <arthurhendges@hotmail.com>
2024-10-12 08:53:20 -0300

    tava tudo cagado

---
+++ lista1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+#include "Heuristics/ManhattanDistance.hpp"
+#include "Puzzles/EightPuzzle.hpp"
+#include "SearchAlg/BFS.hpp"
+
+int main() {
+    printf("Hello, World!\n");
+
+    printf("[%d]\n", BFS(ManhattanDistance(EightPuzzle({0, 1, 2, 3, 4, 5, 6, 7, 8}))).heuristic.Calculate());
+    printf("[%d]\n", BFS(ManhattanDistance(EightPuzzle({1, 0, 2, 3, 4, 5, 6, 7, 8}))).heuristic.Calculate());
+    printf("[%d]\n", BFS(ManhattanDistance(EightPuzzle({1, 2, 0, 3, 4, 5, 6, 7, 8}))).heuristic.Calculate());
+
+    return EXIT_SUCCESS;
+}
