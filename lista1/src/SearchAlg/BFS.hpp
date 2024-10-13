51dafd41cd944757c7ea1433ffa3ecda8b5d3cb4
arthur <arthurhendges@hotmail.com>
2024-10-13 13:56:42 -0300

    bfs ctor

--- lista1/src/SearchAlg/BFS.hpp
+++ lista1/src/SearchAlg/BFS.hpp
@@ -1,13 +1,24 @@
 #pragma once
 
+#include "src/Puzzles/Puzzle.hpp"
+#include "src/Puzzles/EightPuzzle.hpp"
+
 #include <vector>
 
 class BFS {
     public:
-        BFS(std::vector<int> state) : startState(state) {}
+        BFS(std::vector<int> state) 
+        : startState(state),
+        puzzle(EightPuzzle(startState)) { }
+        
+        BFS(const Puzzle& puzzle) 
+        : startState(puzzle.getStartState()),
+        puzzle(puzzle) {}
+        
         void Resolve();
 
         std::vector<int> startState;
     private:
-        std::vector<int> getGoal() { return {0, 1, 2, 3, 4, 5, 6, 7, 8}; };
+        const Puzzle& puzzle;
+        std::vector<int> getGoal() { return puzzle.getGoalState(); }
 };
